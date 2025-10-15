cc84381baf2574755cdb6617f3c347e528a45ffb
Ansh Raut <146610934+Anshraut9@users.noreply.github.com>
2025-10-15 10:46:42 +0530

    Time: 0 ms (100%), Space: 7.7 MB (89.52%) - LeetHub

---
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    long long binomial_coeff(int n, int k) {
+       if(k > n-k) {
+         k = n-k;
+       }
+       long long res = 1;
+       for(int i=0;i<k;i++) {
+        res *= (n-i);
+        res /= (i+1);
+       }
+       return res;
+    }
+    int numTrees(int n) {
+        long long c = binomial_coeff(2*n,n);
+        return c/(n+1);
+    }
+};
